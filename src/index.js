270eeae8ff0486aaaaa5c7cd75bcd06f344e3550
Germán <germancastromontes@gmail.com>
2019-10-14 22:51:22 +0200

    secure app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import bodyParser from 'body-parser';
 
 import routes from './routes';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.json());
 
 app.use('/v1', routes);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,24 @@ const router = Router();
 
 // router.all('*', requireAuthentication)
 router.get('/', (req, res, next) => {
-  res.locals.data = { hello: 'world' };
+  res.locals.data = { action: 'list' };
   next();
 });
+
+router.get('/:id', (req, res, next) => {
+  res.locals.data = { action: 'get' };
+  next();
+});
+
+router.post('/', (req, res, next) => {
+  res.locals.data = { action: 'create' };
+  next();
+});
+
+router.put('/', (req, res, next) => {
+  res.locals.data = { action: 'update' };
+  next();
+});
+
 
 export default router;
