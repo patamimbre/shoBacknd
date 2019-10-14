6994933c2a89cd5e76ba27662142d033f7b66023
Germán <germancastromontes@gmail.com>
2019-10-14 23:35:53 +0200

    setting up mongoose

--- src/config.js
+++ src/config.js
@@ -4,6 +4,9 @@ const { env } = process;
 
 const config = {
   PORT: env.PORT || 3000,
+  MONGO_URL: env.MONGO_URL || 'localhost',
+  MONGO_PORT: env.MONGO_PORT || 27017,
+  MONGO_DB: env.MONGO_DB || 'shop',
 };
 
 export default config;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
@@ -6,8 +7,13 @@ import bodyParser from 'body-parser';
 import routes from './routes';
 import config from './config';
 
-
 const app = express();
+
+mongoose.connect(`mongodb://${config.MONGO_URL}:${config.MONGO_PORT}/${config.MONGO_DB}`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(cors());
 app.use(helmet());
